32977c747d9c7462c7d8fa711ba4c04f0be4fd6d
K. M. Hoodlet <khoodlet@gmail.com>
2017-09-29 21:02:59 -0400

    Fix output variable

--- masscanr.sh
+++ masscanr.sh
@@ -3,4 +3,4 @@
 add=$1
 out=$(echo $add | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
 
-masscan -p1-65535 --rate 10000 $1 -oL $1.txt
+masscan -p1-65535 --rate 10000 $1 -oL $out.txt
