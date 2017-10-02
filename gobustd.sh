5dd29561e44e364119f797b0ab2edd9f49ee0eca
K. M. Hoodlet <khoodlet@gmail.com>
2017-10-01 20:10:44 -0400

    Add filtering for HTTP vs. HTTPS output file names

--- gobustd.sh
+++ gobustd.sh
@@ -4,6 +4,9 @@
 url=$1
 out=$(echo $url | sed 's/https:\/\///;s|\/.*||')
 
+# If the URL was not HTTPS, replace $out
+if [[ $out =~ ^http.*$ ]]; then out=$(echo $url | sed 's/http:\/\///;s|\/.*||'); fi
+
 # SecLists files, symlinked to /root/Web_Content
 files=/root/Web_Content/raft-large-files-lowercase.txt
 direc=/root/Web_Content/raft-large-directories-lowercase.txt
