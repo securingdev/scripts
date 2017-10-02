8732fdf91b63f183afc773a02172fb81eecb421a
K. M. Hoodlet <khoodlet@gmail.com>
2017-10-01 20:11:05 -0400

    Shorten the script to use a single 'uniq' command

--- tldr.sh
+++ tldr.sh
@@ -11,4 +11,5 @@ out=$2
 # Step 5: Profit!
 
 # And this is where the magic happens:
-cat $in | uniq | grep -Eo '(http|https)://[^/"]+' | uniq > $out
+#cat $in | uniq | grep -Eo '(http|https)://[^/"]+' | uniq > $out
+cat $in | grep -Eo '(http|https)://[^/"]+' | uniq > $out
