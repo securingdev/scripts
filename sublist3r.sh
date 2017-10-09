862c9054c5ac6634979164582840daa7703008a3
K. M. Hoodlet <khoodlet@gmail.com>
2017-10-09 10:16:15 -0400

    Update file saving mechanism

--- sublist3r.sh
+++ sublist3r.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-/usr/bin/python ~/Sublist3r/sublist3r.py -t 20 -d $1 -o $2 
+#/usr/bin/python ~/Sublist3r/sublist3r.py -d $1 -p $2 -o $3
+#/usr/bin/python ~/Sublist3r/sublist3r.py -t 5 -d $1 -o $2 
+/usr/bin/python ~/tools/Sublist3r/sublist3r.py -t 5 -d $1 -o $1.txt
