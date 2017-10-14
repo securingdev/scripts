bc0cf3bcf84bc5392ca69f558a02d732332e87ee
K. M. Hoodlet <khoodlet@gmail.com>
2017-10-14 19:40:20 -0400

    Update speed

--- masscanr.sh
+++ masscanr.sh
@@ -3,6 +3,4 @@
 add=$1
 out=$(echo $add | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
 
-#masscan -p1-65535 --maxrate 1000 $1 -oL $out.txt
-#masscan -p1-65535 --rate 10000 $1 -oL $out.txt
-masscan -p1-65535 --rate 5000 $1 -oL $out.txt
+masscan -p1-65535 --rate 5000 $1 -oL $out.txt && 
