548cf3fbe7a0eeddec1aca5af9f59f07d4c36db2
K. M. Hoodlet <khoodlet@gmail.com>
2017-10-15 19:55:48 -0400

    Add HTTPS support

--- wpscan.sh
+++ wpscan.sh
@@ -3,4 +3,4 @@
 url=$1
 out=$(echo $1 | sed 's/https:\/\///;s|\/.*||')
 
-docker run -it --rm wpscanteam/wpscan -u $url | tee -a wpscan.$out.txt
+docker run -it --rm wpscanteam/wpscan --disable-tls-checks -u $url | tee -a wpscan.$out.txt
