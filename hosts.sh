c93b31a12925a2610a9706e0dc2bc3fae0a07ff2
K. M. Hoodlet <khoodlet@gmail.com>
2017-11-21 18:56:10 -0500

    Replaced with a bash one-liner... for great justice

--- hosts.sh
+++ hosts.sh
@@ -2,19 +2,5 @@
 
 in=$1
 out=hosts.$1
-dns=(8.8.8.8 8.8.4.4 208.67.220.220 208.67.222.222)
 
-while read line; do
-        rnum=$[($RANDOM % 4)]
-        resolver=${dns[$rnum]}
-        ip=$(dig +short @$resolver $line)
-
-        echo ""
-        echo "Resolver: " $resolver && \
-        echo "Domain: " $line && \
-        echo "IP: " $ip && \
-        echo "" && \
-        echo $line $ip >> $out && \
-        sleep 1 && \
-        sleep $[($RANDOM % 3)]
-done < $1
+dig -f $in +noall +short | tee -a $out
