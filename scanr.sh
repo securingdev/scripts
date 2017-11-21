b22f005c82ccc59ffb9b36f3e0f4397fec2489f1
K. M. Hoodlet <khoodlet@gmail.com>
2017-11-21 17:05:59 -0500

    Add scanr.sh helper script

---
+++ scanr.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+in=$1
+out=masscan.$in
+
+while read line; do
+        echo $line >> $out
+        ip=$(echo $line | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1)
+        
+        if [ -z "$ip" ]; 
+            then
+                continue
+        else
+            echo "" && \
+            echo "Scanning IP: " $ip && \
+            echo "" && \
+            masscan -p1-65535 --rate 5000 $ip -oL $ip.txt && \
+            cat $ip.txt >> $out && rm $ip.txt &&\
+            echo "" >> $out && \
+            sleep $[($RANDOM %3)]
+        fi
+done < $in
