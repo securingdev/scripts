9da930d326a3548ee796d40b1cdb8a255a46821e
K. M. Hoodlet <khoodlet@gmail.com>
2017-11-24 15:12:26 -0500

    Update scanr with a commented field to remove the read-in file

--- scanr.sh
+++ scanr.sh
@@ -14,9 +14,11 @@ while read line; do
             echo "" && \
             echo "IP: " $ip && \
             echo "" && \
-            masscan -p1-65535 --rate 5000 $ip -oL $ip.txt && \
+            masscan -p1-65535 --rate 1000 $ip -oL $ip.txt && \
             cat $ip.txt >> $out && rm $ip.txt &&\
             echo "" >> $out && \
             sleep $[($RANDOM %3)]
         fi
 done < $in
+
+#rm $1
