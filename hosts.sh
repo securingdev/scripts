7a8bee05b29077e077cc42ecb7cea42b8c535b15
K. M. Hoodlet <khoodlet@gmail.com>
2017-11-24 15:11:47 -0500

    Update hosts to write out to two files

--- hosts.sh
+++ hosts.sh
@@ -4,6 +4,5 @@ in=$1
 temp=temp.$1
 out=hosts.$1
 
-dig -f $in +noall +short | tee -a $temp && \
-uniq -u $temp > $out && \
-rm $temp
+dig -f $in +noall +short | tee -a $out && \
+cat $out | grep '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | sort | uniq > $temp
